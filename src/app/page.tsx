320a35a63a0940584dcf1abfef663bf69f365248
Emma Baka <112560476+emmabaka@users.noreply.github.com>
2023-09-25 17:37:44 +0300

    feat: add form for add new expense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
 import Card from "@/components/Card/Card";
 import s from "./page.module.scss";
+import AddExpenseForm from "@/components/AddExpenseForm/AddExpenseForm";
 
 export default function Home() {
-  return <Card/> ;
+  return (
+    <>
+      <Card />
+      <AddExpenseForm />
+    </>
+  );
 }
---
+++ src/components/AddExpenseForm/AddExpenseForm.tsx
@@ -0,0 +1,70 @@
+"use client";
+import { ArrowSVG } from "../svgs/svgs";
+import s from "./AddExpenseForm.module.scss";
+import { useState } from "react";
+
+const AddExpenseForm = () => {
+  const currDate = new Date()
+    .toLocaleDateString()
+    .split(".")
+    .toReversed()
+    .join("-");
+
+  const [date, setDate] = useState(currDate);
+  const [category, setCategory] = useState("Coffe & Tea");
+  const [amount, setAmount] = useState("");
+
+  return (
+    <form className={s.form}>
+      <div className={s.inputWrap}>
+        <label className={s.label} htmlFor="category">
+          Category
+        </label>
+        <select
+          className={`${s.input} ${s.select}`}
+          name="category"
+          id="category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        >
+          <option value="Coffe & Tea">Coffe & Tea</option>
+          <option value="Car">Car</option>
+        </select>
+        <div className={s.arrow}>
+          <ArrowSVG />
+        </div>
+      </div>
+      <div className={s.inputWrap}>
+        <label className={s.label} htmlFor="amount">
+          Amount
+        </label>
+        <input
+          className={`${s.input} ${s.amount}`}
+          type="number"
+          id="amount"
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <span className={s.dollar}>$</span>
+      </div>
+      <div className={s.inputWrap}>
+        <label className={s.label} htmlFor="date">
+          Date
+        </label>
+        <input
+          className={`${s.input} ${s.date}`}
+          type="date"
+          name="date"
+          id="date"
+          value={date}
+          onChange={(e) => setDate(e.target.value)}
+        />
+        <div className={s.selectedDate}>
+          {new Date(date).toUTCString().slice(0, 16)}
+        </div>
+      </div>
+    </form>
+  );
+};
+
+export default AddExpenseForm;
