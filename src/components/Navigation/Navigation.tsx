062191314db35070b1f782f54ce13073667a72e4
Emma Baka <112560476+emmabaka@users.noreply.github.com>
2023-09-25 13:38:30 +0300

    feat: add navigation

---
+++ src/components/Navigation/Navigation.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { HomeSVG, StatsSVG, WalletSVG, SettingsSVG } from "../svgs/svgs.js";
+import s from "./Navigation.module.scss";
+
+const Navigation = () => {
+  return (
+    <footer className={s.footer}>
+      <div className="container">
+        <nav className={s.nav}>
+          <ul className={s.navList}>
+            <li>
+              <Link className={s.link} href="/">
+                <HomeSVG />
+              </Link>
+            </li>
+            <li>
+              <Link className={s.link} href="/statistics">
+                <StatsSVG />
+              </Link>
+            </li>
+            <li>
+              <Link className={s.link} href="/wallet">
+                <WalletSVG />
+              </Link>
+            </li>
+            <li>
+              <Link className={s.link} href="/settings">
+                <SettingsSVG />
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
+};
+
+export default Navigation;
