e6d7ff5605b89f96aac218f42ce9653f7a7fe943
Emma Baka <112560476+emmabaka@users.noreply.github.com>
2023-10-10 12:15:49 +0300

    feat: add clear all data function

--- src/components/Settings/Settings.tsx
+++ src/components/Settings/Settings.tsx
@@ -2,10 +2,23 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { auth } from "@/firebase";
+import { auth, db } from "@/firebase";
 import { getAuth, signOut } from "firebase/auth";
+import { collection, deleteDoc, doc, updateDoc } from "firebase/firestore";
+import { getHistory } from "@/utils/getHistory";
+import { getTotal } from "@/utils/getTotal";
 import clsx from "clsx";
 import s from "./Settings.module.scss";
+
+interface Transaction {
+  amount: string;
+  category: string;
+  date: string;
+  createdAt: string;
+  status: string;
+  total: string;
+  id: string;
+}
 
 const Settings = () => {
   const [userEmail, setUserEmail] = useState<string | null>(null);
@@ -18,9 +31,7 @@ const Settings = () => {
     await signOut(auth);
     router.push("/login");
   };
-  console.log(auth);
-
-  const deleteCollection = () => {};
+  // console.log(auth);
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
@@ -29,6 +40,38 @@ const Settings = () => {
       }
     });
   }, []);
+
+  const deleteDocs = async () => {
+    try {
+      auth.onAuthStateChanged(async (user) => {
+        if (user) {
+          const transactionsCollectionRef = collection(db, user.uid);
+          const transactionsCollectionTotalRef = collection(
+            db,
+            `total${user.uid}`
+          );
+
+          const history = (await getHistory(
+            transactionsCollectionRef
+          )) as Transaction[];
+
+          history
+            .map((item) => item.id)
+            .forEach(async (item: string) => {
+              await deleteDoc(doc(db, user.uid, item));
+            });
+
+          const data = await getTotal(transactionsCollectionTotalRef);
+          const totalDoc = doc(db, `total${auth.currentUser!.uid}`, data[0].id);
+          await updateDoc(totalDoc, {
+            total: 0,
+          });
+        }
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <div className="box">
@@ -65,7 +108,7 @@ const Settings = () => {
           <button
             className={clsx(s.clearButton, s.confirm)}
             onClick={() => {
-              deleteCollection();
+              deleteDocs();
               setClear(false);
             }}
           >
